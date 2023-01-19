a1ab0fc8e9061ad824678a22a7bd71bdb28ae3e7
Abizrh <abizarah857@gmail.com>
2023-01-19 18:45:20 +0700

    clean up

--- client/index.js
+++ client/index.js
@@ -5,32 +5,4 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-// Iini jadalah consth alkakldsjfklsjdkfjsdlkfjldskjflksdjflkjsdf
-// lajdsfklsjlkfjsklfjsdjf jsdlkjf lsdjf sdljf
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj   
-// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
-
 AppRegistry.registerComponent(appName, () => App);
