2990d392631b90420dfadca201675bc94184da64
Abizrh <abizarah857@gmail.com>
2023-01-19 18:10:23 +0700

    coba

---
+++ client/index.js
@@ -0,0 +1,29 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+// Iini jadalah consth alkakldsjfklsjdkfjsdlkfjldskjflksdjflkjsdf
+// lajdsfklsjlkfjsklfjsdjf jsdlkjf lsdjf sdljf
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+// jaldjflksdjfk jsdlfjsdlkf jsdklfj sdf sdfj
+
+AppRegistry.registerComponent(appName, () => App);
