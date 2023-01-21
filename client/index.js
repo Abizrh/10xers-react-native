4f76fc73faf9a14eacf77bbb379e99555e2a403d
Abizrh <abizarah857@gmail.com>
2023-01-21 17:26:37 +0700

    fix (test/index): trying to use git aliases

--- client/index.js
+++ client/index.js
@@ -5,4 +5,14 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+/**
+ * sdfkls
+ * sdflksdjfsflsd
+ * fsfksdjfskf
+ * sfjakua
+ * akaua a
+ * akaua
+ * acoba coa
+ * 
+ */
 AppRegistry.registerComponent(appName, () => App);
